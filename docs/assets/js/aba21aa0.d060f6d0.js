21cf8c86856fa8eadf23a3330cdc923cbf273b83
John Doe <john.doe@example.com>
2025-07-05 12:23:48 -0700

    remove docusarus

--- docs/assets/js/aba21aa0.d060f6d0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[742],{4630:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- website/test_pandoc-installer-wrapper.js
+++
@@ -1,5 +0,0 @@
-const { runPandoc } = require('pandoc-installer-wrapper');
-
-runPandoc(['--version'])
-  .then(output => console.log('Pandoc version:', output))
-  .catch(err => console.error('Error running Pandoc:', err));
